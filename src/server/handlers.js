c880ff7b14766e4e899c7bf36a7172761b67a529
leopasakdal <leo.pasakdal@accedo.tv>
2019-02-01 02:18:25 +0800

    cleanups

--- src/server/handlers.js
+++ src/server/handlers.js
@@ -1,22 +1,19 @@
-const GAME_CONNECTION_REQUEST = 'socket/GAME_CONNECTION_REQUEST';
-const START_GAME_REQUEST = 'socket/START_GAME_REQUEST';
-const INPUT_REQUEST = 'socket/INPUT_REQUEST';
 const EXIT_GAME_REQUEST = 'socket/EXIT_GAME_REQUEST';
+const GAME_CONNECTION_REQUEST = 'socket/GAME_CONNECTION_REQUEST';
+const INPUT_REQUEST = 'socket/INPUT_REQUEST';
+const START_GAME_REQUEST = 'socket/START_GAME_REQUEST';
 
 const gameManager = require('./manager/GameManager');
 
 const connectGame = (context, action) => {
-  console.info('connecting to game...');
   gameManager.connect(context, action);
 };
 
 const startGame = context => {
-  console.info('request to start the game...');
   gameManager.start(context);
 };
 
 const inputRequest = (context, action) => {
-  console.info('input received from client');
   gameManager.processInput(context, action);
 };
 
@@ -26,7 +23,7 @@ const exitRequest = (context, action) => {
 
 module.exports = {
   [INPUT_REQUEST]: inputRequest,
+  [EXIT_GAME_REQUEST]: exitRequest,
   [GAME_CONNECTION_REQUEST]: connectGame,
-  [START_GAME_REQUEST]: startGame,
-  [EXIT_GAME_REQUEST]: exitRequest
+  [START_GAME_REQUEST]: startGame
 };
