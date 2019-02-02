563b93dda0a3ba8da54352f249a318c21e3498c1
leopasakdal <leo.pasakdal@accedo.tv>
2019-02-02 13:46:16 +0800

    added languages

--- src/server/helpers/language.js
+++ src/server/helpers/language.js
@@ -1,5 +1,9 @@
 const ERROR = {
-  IN_PROGRESS: 'Game in progress'
+  IN_PROGRESS: 'Game in progress',
+  INVALID_CLIENT: 'Invalid client',
+  INVALID_INPUT: 'Invalid input',
+  TURN_DISABLED: 'Not your turn yet',
+  PLAYER_LEFT_GAME: 'Other player has left the game'
 };
 
 module.exports = { ERROR };
